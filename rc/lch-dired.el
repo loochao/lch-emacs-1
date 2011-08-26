2a91c41518908c6317efe487b16ec57c50b3a1ae
Chao LU <loochao@gmail.com>
2011-08-25 20:23:10 -0400

    dired sort, dir first.

--- rc/lch-dired.el
+++ rc/lch-dired.el
@@ -26,6 +26,19 @@
 (ignore-errors (require 'emms-player-mplayer))
 ;(require 'dired-sort-menu)
 
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+  (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
 ;>-- Allows recursive deletes --<;
 ;(setq dired-recursive-deletes 'top)
 (setq dired-recursive-deletes 'always)
